99a8f745f5062db0bcbf18974751491554412bd0
Maggie Paquette <paqu0424@algonquinlive.com>
2022-04-19 12:53:14 -0400

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+const container = document.getElementById('container');
+
+// GET THE REFERENCES
+const links = document.querySelectorAll('[data-ajax-link]');
+let url = location.href;
+
+const loadContent = async (href) => {
+  const parsedUrl = new URL(href);
+  const hash = parsedUrl.hash || '#_home.html';
+  const partial = await fetch(`/partials/${hash.slice(1)}`);
+  container.innerHTML = await partial.text();
+};
+loadContent(url);
+
+const selectContent = (link) => {
+  console.log(link.href);
+  loadContent(link.href);
+};
+links.forEach((link) =>
+  link.addEventListener('click', () => selectContent(link))
+);
