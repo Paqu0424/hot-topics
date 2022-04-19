b8f73b6fffcddc946742dbd367497800b932bf4c
Maggie Paquette <paqu0424@algonquinlive.com>
2022-04-19 14:27:16 -0400

    commit changes

--- javascript/script.js
+++ javascript/script.js
@@ -5,15 +5,18 @@ const links = document.querySelectorAll('[data-ajax-link]');
 let url = location.href;
 
 const loadContent = async (href) => {
-  const parsedUrl = new URL(href);
-  const hash = parsedUrl.hash || '#home.html';
-  const partial = await fetch(`partials/${hash.slice(1)}`);
-  container.innerHTML = await partial.text();
+  try {
+    const parsedUrl = new URL(href);
+    const hash = parsedUrl.hash || '#home.html';
+    const partial = await fetch(`partials/${hash.slice(1)}`);
+    container.innerHTML = await partial.text();
+  } catch (err) {
+    console.err(err);
+  }
 };
 loadContent(url);
 
 const selectContent = (link) => {
-  console.log(link.href);
   loadContent(link.href);
 };
 links.forEach((link) =>
